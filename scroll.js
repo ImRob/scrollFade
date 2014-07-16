81d3b2a0121c2933d0eada6a642b29793d9b01d2
ImRob <rob.burkins@gmail.com>
2014-07-16 17:09:23 -0400

    Create scroll.js

---
+++ scroll.js
@@ -0,0 +1,33 @@
+$(window).on('scroll',function(){
+  var list=$('article');
+  var listArr=makeAnArray(list);
+  var scrollDist=$(window).scrollTop();
+  var winHeight=$(window).height();
+  var winBottom=winHeight+scrollDist;
+  var headHeight=$('.header').height();
+  var heroHeight=$('.hero').height();
+  
+  function makeAnArray(obj){
+    return [].map.call(obj,function(el){
+      return el;
+  });
+  }
+  
+   if(scrollDist>heroHeight){
+		$('.header').addClass('fixed');
+		$('.placeHolder').css('display','block');
+		listArr.forEach(function(el,ind){
+      		var elTop=$(el).offset().top;
+      		var elBottom=elTop + $(el).height();
+  				console.log(listArr);
+			if(elTop <= (scrollDist + headHeight) && (elBottom>=scrollDist) ){
+				$('li.active').removeClass('active');
+				$('li').eq(ind).addClass('active');
+			}
+		});
+   		}else{
+				$('li .active').removeClass('active'); 
+				$('.header').removeClass('fixed');
+				$('.placeHolder').css('display','none');
+			}  
+});
